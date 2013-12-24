97381e5642770efc344d5e0321ef2591ed8b421e
Della Wheeler <dellajeanwheeler@gmail.com>
2013-12-23 18:41:40 -0600

    added slider with custom pager and started names array

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+firstnames = ["della", "justin"]
+lastnames = ["wheeler", "threlkeld"]
+fullname = function(i){
+	return firstnames[i] +" "+ lastnames[i]
+}
+
+pagers = function(i){
+	slideIndex = i + 1
+	text= $('#js-slide-' +slideIndex).find('h1').text()
+	return 'go to <i>'+text+'</i>'
+}
+
+$('#js--slider').bxSlider({
+	buildPager: pagers
+})
+
+
 
